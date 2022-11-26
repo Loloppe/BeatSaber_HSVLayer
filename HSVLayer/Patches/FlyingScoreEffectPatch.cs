2d00f621ccd11620fcb4a2bac007206560709f2f
Laurent Blais-Côté <LaurentBlaisCote@outlook.com>
2022-11-25 19:00:31 -0500

    init

---
+++ HSVLayer/Patches/FlyingScoreEffectPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace HSVLayer.Patches
+{
+	[HarmonyPatch(typeof(EffectPoolsManualInstaller), nameof(EffectPoolsManualInstaller.ManualInstallBindings))]
+	static class FlyingScoreEffectPatch
+	{
+		[HarmonyPriority(int.MinValue)]
+		static void Prefix(FlyingScoreEffect ____flyingScoreEffectPrefab)
+		{
+			var hsv = ____flyingScoreEffectPrefab.gameObject;
+			int layer = 8;
+			hsv.layer = layer;
+			foreach (Transform t in hsv.GetComponentsInChildren<Transform>())
+			{
+				t.gameObject.layer = layer;
+			}
+		}
+	}
+}
---
+++ HSVLayer/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using IPA;
+using System.Reflection;
+using IPALogger = IPA.Logging.Logger;
+
+namespace HSVLayer
+{
+	[Plugin(RuntimeOptions.DynamicInit)]
+	public class Plugin
+	{
+		internal static Plugin Instance;
+		internal static IPALogger Log;
+		internal static Harmony harmony;
+
+		[Init]
+		public Plugin(IPALogger logger)
+		{
+			Instance = this;
+			Log = logger;
+			harmony = new Harmony("Loloppe.BeatSaber.HSVLayer");
+		}
+
+		[OnEnable]
+		public void OnEnable()
+		{
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+			Log.Warn("patched");
+		}
+
+		[OnDisable]
+		public void OnDisable()
+		{
+			harmony.UnpatchSelf();
+		}
+	}
+}
